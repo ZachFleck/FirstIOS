1e2af9e3c2c20a0e25ea5daabcaafb529d746705
ZachFleck <zacfleck10@gmail.com>
2018-10-11 12:07:34 -0600

    made the color to class

--- FirstIOS/Model/ColorTool.swift
+++ FirstIOS/Model/ColorTool.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import UIKit
+
+public class ColorTool
+{
+    public func createRandomColor() -> UIColor
+    {
+        let redPercent : CGFloat = CGFloat(Double(arc4random() % 256) / 255.00)
+        let bluePercent : CGFloat = CGFloat (Double(arc4random() % 256) / 255.00)
+        let greenPercent : CGFloat = CGFloat (Double(arc4random() % 256) / 255.00)
+        
+        
+        return UIColor(red:redPercent,green:greenPercent,blue:bluePercent,alpha:CGFloat(100))
+    }
+}
